5192dc4383b671eb39ea5b7b93652ab9821d933b
Eduardo Sandalo Porto <edu@sandalo.dev>
2024-08-22 22:10:54 -0300

    Prepare for initial release
    What's left:
    - Publish to crates-io
    - Publish release on GitHub
    - Pull language server binary from crates-io
    - Release to VSCode

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and both this changelog and the crate's versioning scheme follow what the
+[Bend repository](https://github.com/HigherOrderCO/Bend) is using at the moment.
+
+## [Unreleased]
+
+All current features are yet unreleased on a stable version, and are only available in the `0.2.37` alpha versions.
+
+## [0.2.37-alpha.1] - 2024-08-23
+
+First release!
+
+### Added
+
+- Semantic token highlighting through tree-sitter
+- Diagnostic reporting
+
+<!-- still haven't add a release to GitHub -->
+[0.2.37-alpha.1]: https://github.com/HigherOrderCO/bend-language-server/
+[Unreleased]: https://github.com/HigherOrderCO/bend-language-server/
+
+# Collaborators
+
+This project got inspiration from [Pedro Braga](https://github.com/mrpedrobraga)'s initial language server implementation and [Rohan Vashisht](https://github.com/RohanVashisht1234)'s VSCode extension. Thank you for your collaboration!
--- editors/code/src/extension.ts
+++ editors/code/src/extension.ts
@@ -4,168 +4,177 @@
  * ------------------------------------------------------------------------------------------ */
 
 import {
-    languages,
-    workspace,
-    EventEmitter,
-    ExtensionContext,
-    window,
-    InlayHintsProvider,
-    TextDocument,
-    CancellationToken,
-    Range,
-    InlayHint,
-    TextDocumentChangeEvent,
-    ProviderResult,
-    commands,
-    WorkspaceEdit,
-    TextEdit,
-    Selection,
-    Uri,
-  } from "vscode";
-  
-  import {
-    Disposable,
-    Executable,
-    LanguageClient,
-    LanguageClientOptions,
-    ServerOptions,
-  } from "vscode-languageclient/node";
+  languages,
+  workspace,
+  EventEmitter,
+  ExtensionContext,
+  window,
+  InlayHintsProvider,
+  TextDocument,
+  CancellationToken,
+  Range,
+  InlayHint,
+  TextDocumentChangeEvent,
+  ProviderResult,
+  commands,
+  WorkspaceEdit,
+  TextEdit,
+  Selection,
+  Uri,
+} from "vscode";
 
-  let client: LanguageClient;
+import {
+  Disposable,
+  Executable,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export async function activate(context: ExtensionContext) {
+  // let disposable = commands.registerCommand("helloworld.helloWorld", async uri => {
+  //   // The code you place here will be executed every time your command is executed
+  //   // Display a message box to the user
+  //   let document = await workspace.openTextDocument(uri);
+  //   await window.showTextDocument(document);
+  //   // console.log(uri)
+  //   window.activeTextEditor.document
+  //   let editor = window.activeTextEditor;
+  //   let range = new Range(1, 1, 1, 1)
+  //   editor.selection = new Selection(range.start, range.end);
+  // });
+  // context.subscriptions.push(disposable);
+
+  const traceOutputChannel = window.createOutputChannel("Bend Language Server trace");
+  const command = process.env.SERVER_PATH || "bend-language-server";
+
+  // We have to check if `bend-language-server` is installed, and if it's not, try to install it.
   
-  export async function activate(context: ExtensionContext) {
-    let disposable = commands.registerCommand("helloworld.helloWorld", async uri => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      let document = await workspace.openTextDocument(uri);
-      await window.showTextDocument(document);
-      
-      // console.log(uri)
-      window.activeTextEditor.document
-      let editor = window.activeTextEditor;
-      let range = new Range(1, 1, 1, 1)
-      editor.selection = new Selection(range.start, range.end);
-    });
-  
-    context.subscriptions.push(disposable);
-  
-    const traceOutputChannel = window.createOutputChannel("Bend Language Server trace");
-    const command = process.env.SERVER_PATH || "bend-language-server";
-    const run: Executable = {
-      command,
-      options: {
-        env: {
-          ...process.env,
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          RUST_LOG: "debug",
-        },
+
+  const run: Executable = {
+    command,
+    options: {
+      env: {
+        ...process.env,
       },
-    };
-    const serverOptions: ServerOptions = {
-      run,
-      debug: run,
-    };
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    // Options to control the language client
-    let clientOptions: LanguageClientOptions = {
-      // Register the server for plain text documents
-      documentSelector: [{ scheme: "file", language: "bend" }],
-      synchronize: {
-        // Notify the server about file changes to '.clientrc files contained in the workspace
-        fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+  const debug: Executable = {
+    command,
+    options: {
+      env: {
+        ...process.env,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        RUST_LOG: "debug",
       },
-      traceOutputChannel,
-    };
-  
-    // Create the language client and start the client.
-    client = new LanguageClient("bend-language-server", "Bend Language Server", serverOptions, clientOptions);
-    // activateInlayHints(context);
-    client.start();
+    },
+  };
+  const serverOptions: ServerOptions = {
+    run,
+    debug,
+  };
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: "file", language: "bend" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+    traceOutputChannel,
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient("bend-language-server", "Bend Language Server", serverOptions, clientOptions);
+  // activateInlayHints(context);
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
   }
-  
-  export function deactivate(): Thenable<void> | undefined {
-    if (!client) {
-      return undefined;
-    }
-    return client.stop();
-  }
-  
-  export function activateInlayHints(ctx: ExtensionContext) {
-    const maybeUpdater = {
-      hintsProvider: null as Disposable | null,
-      updateHintsEventEmitter: new EventEmitter<void>(),
-  
-      async onConfigChange() {
-        this.dispose();
-  
-        const event = this.updateHintsEventEmitter.event;
-        this.hintsProvider = languages.registerInlayHintsProvider(
-          { scheme: "file", language: "bend" },
-          new (class implements InlayHintsProvider {
-            onDidChangeInlayHints = event;
-            resolveInlayHint(hint: InlayHint, token: CancellationToken): ProviderResult<InlayHint> {
-              const ret = {
-                label: hint.label,
-                ...hint,
-              };
-              return ret;
-            }
-            async provideInlayHints(
-              document: TextDocument,
-              range: Range,
-              token: CancellationToken
-            ): Promise<InlayHint[]> {
-              const hints = (await client
-                .sendRequest("custom/inlay_hint", { path: document.uri.toString() })
-                .catch(err => null)) as [number, number, string][];
-              if (hints == null) {
-                return [];
-              } else {
-                return hints.map(item => {
-                  const [start, end, label] = item;
-                  let startPosition = document.positionAt(start);
-                  let endPosition = document.positionAt(end);
-                  return {
-                    position: endPosition,
-                    paddingLeft: true,
-                    label: [
-                      {
-                        value: `${label}`,
-                        location: {
-                          uri: document.uri,
-                          range: new Range(1, 0, 1, 0)
-                        },
-                        command: {
-                          title: "hello world",
-                          command: "helloworld.helloWorld",
-                          arguments: [document.uri],
-                        },
+  return client.stop();
+}
+
+export function activateInlayHints(ctx: ExtensionContext) {
+  const maybeUpdater = {
+    hintsProvider: null as Disposable | null,
+    updateHintsEventEmitter: new EventEmitter<void>(),
+
+    async onConfigChange() {
+      this.dispose();
+
+      const event = this.updateHintsEventEmitter.event;
+      this.hintsProvider = languages.registerInlayHintsProvider(
+        { scheme: "file", language: "bend" },
+        new (class implements InlayHintsProvider {
+          onDidChangeInlayHints = event;
+          resolveInlayHint(hint: InlayHint, token: CancellationToken): ProviderResult<InlayHint> {
+            const ret = {
+              label: hint.label,
+              ...hint,
+            };
+            return ret;
+          }
+          async provideInlayHints(
+            document: TextDocument,
+            range: Range,
+            token: CancellationToken
+          ): Promise<InlayHint[]> {
+            const hints = (await client
+              .sendRequest("custom/inlay_hint", { path: document.uri.toString() })
+              .catch(err => null)) as [number, number, string][];
+            if (hints == null) {
+              return [];
+            } else {
+              return hints.map(item => {
+                const [start, end, label] = item;
+                let startPosition = document.positionAt(start);
+                let endPosition = document.positionAt(end);
+                return {
+                  position: endPosition,
+                  paddingLeft: true,
+                  label: [
+                    {
+                      value: `${label}`,
+                      location: {
+                        uri: document.uri,
+                        range: new Range(1, 0, 1, 0)
                       },
-                    ],
-                  };
-                });
-              }
+                      command: {
+                        title: "hello world",
+                        command: "helloworld.helloWorld",
+                        arguments: [document.uri],
+                      },
+                    },
+                  ],
+                };
+              });
             }
-          })()
-        );
-      },
-  
-      onDidChangeTextDocument({ contentChanges, document }: TextDocumentChangeEvent) {
-        // debugger
-        // this.updateHintsEventEmitter.fire();
-      },
-  
-      dispose() {
-        this.hintsProvider?.dispose();
-        this.hintsProvider = null;
-        this.updateHintsEventEmitter.dispose();
-      },
-    };
-  
-    workspace.onDidChangeConfiguration(maybeUpdater.onConfigChange, maybeUpdater, ctx.subscriptions);
-    workspace.onDidChangeTextDocument(maybeUpdater.onDidChangeTextDocument, maybeUpdater, ctx.subscriptions);
-  
-    maybeUpdater.onConfigChange().catch(console.error);
-  }
-  
+          }
+        })()
+      );
+    },
+
+    onDidChangeTextDocument({ contentChanges, document }: TextDocumentChangeEvent) {
+      // debugger
+      // this.updateHintsEventEmitter.fire();
+    },
+
+    dispose() {
+      this.hintsProvider?.dispose();
+      this.hintsProvider = null;
+      this.updateHintsEventEmitter.dispose();
+    },
+  };
+
+  workspace.onDidChangeConfiguration(maybeUpdater.onConfigChange, maybeUpdater, ctx.subscriptions);
+  workspace.onDidChangeTextDocument(maybeUpdater.onDidChangeTextDocument, maybeUpdater, ctx.subscriptions);
+
+  maybeUpdater.onConfigChange().catch(console.error);
+}
