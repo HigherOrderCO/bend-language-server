f94adc3a18d11dfe7d48996b38c09eeac9c7ccde
Eduardo Sandalo Porto <edu@sandalo.dev>
2024-08-27 11:50:07 -0300

    Add `--version` argument to the language server

--- src/bin/bend-language-server.rs
+++ src/bin/bend-language-server.rs
@@ -1,3 +1,6 @@
+use std::env;
+
+use tokio::io::AsyncWriteExt;
 use tower_lsp::{LspService, Server};
 
 use bend_language_server::server::*;
@@ -5,7 +8,16 @@ use bend_language_server::server::*;
 #[tokio::main]
 async fn main() {
     let stdin = tokio::io::stdin();
-    let stdout = tokio::io::stdout();
+    let mut stdout = tokio::io::stdout();
+
+    if env::args().any(|arg| arg == "--version") {
+        let version = format!("{}\n", env!("CARGO_PKG_VERSION"));
+        stdout
+            .write_all(version.as_bytes())
+            .await
+            .expect("Couldn't write to stdout.");
+        return;
+    }
 
     let (service, socket) = LspService::new(Backend::new);
     Server::new(stdin, stdout, socket).serve(service).await;
