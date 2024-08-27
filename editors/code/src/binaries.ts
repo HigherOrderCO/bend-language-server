b9f5a3b59fa99c291bff9b9ce4f4389ada4edc50
Eduardo Sandalo Porto <edu@sandalo.dev>
2024-08-27 20:18:27 -0300

    Using `fp-ts` checkpoint

--- editors/code/src/binaries.ts
+++ editors/code/src/binaries.ts
@@ -6,6 +6,8 @@
  * which is also licensed under the MIT license.
  * ------------------------------------------------------------------------------------------ */
 
+// This module deals with pulling and installing the language server binaries into the system.
+
 import { ConfigurationTarget, ExtensionContext, ProgressLocation, window, workspace, WorkspaceFolder } from "vscode";
 import { Logger } from "vscode-languageclient";
 import { match } from 'ts-pattern';
@@ -15,8 +17,14 @@ import * as fs from "fs";
 import * as os from "os";
 import which from "which";
 import dedent from "dedent-js";
+import path from "path";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
 
-// This module deals with pulling and installing the language server binaries into the system.
+const BEND_LS_EXE = "bend-language-server";
+// On Windows, the executable needs to be stored with a .exe extension
+const EXT = process.platform === "win32" ? ".exe" : "";
 
 // The language server can be installed automatically by the extension, be used from the PATH environment
 // variable, or configured from a specific location by the user
@@ -24,9 +32,6 @@ type ManageLanguageServer = "automatic" | "PATH";
 let manageLS = workspace
   .getConfiguration("bend")
   .get("manageLanguageServer") as ManageLanguageServer;
-
-// On Windows, the executable needs to be stored somewhere with an .exe extension
-const executableExtension = process.platform === "win32" ? ".exe" : "";
 
 type ProcessCallback = (
   error: child_process.ExecFileException | null,
@@ -45,7 +50,7 @@ export async function findLanguageServer(
   logger: Logger,
   workingDir: string,
   folder?: WorkspaceFolder
-): Promise<[string, string | undefined]> {
+): Promise<E.Either<string, string>> {
   logger.info("Looking for the language server");
 
   let serverExecutablePath = workspace
@@ -53,8 +58,7 @@ export async function findLanguageServer(
     .get("serverExecutablePath") as string;
   if (serverExecutablePath) {
     // Get the language server from the configured location
-    const executable = await findServerExecutable(logger, folder);
-    return [executable, undefined];
+    return await findConfiguredServerExecutable(logger, folder);
   }
 
   const storagePath = await getStoragePath(context);
@@ -88,58 +92,158 @@ export async function findLanguageServer(
     context.globalState.update("pluginInitialized", true);
   }
 
+  // User wants us to grab `bend-language-server` from the PATH environment variable
   if (manageLS == "PATH") {
-    const executable = await findServerExecutableinPATH(context, logger);
-    return [executable, undefined];
+    return await findExecutableInPATH(BEND_LS_EXE, context, logger);
   }
 
-  // If we're here, automatically install the latest `bend-language-server` binary.
+  // Automatically install the latest `bend-language-server` binary.
+  if (manageLS == "automatic") {
+    // -> check if installed locally
+    //   -> yes: check if latest version
+    //     -> yes: ask to update
+    //       -> yes: update and run
+    //       -> no: just run
+    //     -> no: just run
+    //   -> no: install and run
 
+    let languageServer = await managedServerExecutableLocation(context);
+    if (!executableExists(languageServer)) {
+      // Install the `bend-language-server` binary.
+      return pipe(
+        await getLanguageServerLatestVersion(context, logger),
+        E.chain(version => installLanguageServer(version, context, logger))
+      )
+    } else {
+      // Check if latest version is installed.
+      const newVersion = await getLanguageServerLatestVersion(context, logger);
 
+      const currentVersion =
+        (await callAsync(
+          languageServer,
+          ["--version"],
+          logger,
+          undefined,
+          "Checking current installed version...",
+          true
+        )).trim();
 
-  throw "TODO";
+      return await pipe(newVersion,
+        E.match(
+          async _error => {
+            // We couldn't find the newest version for some reason...
+            // Keep this one.
+            return E.right(languageServer);
+          },
+          async version => {
+            if (version !== currentVersion) {
+              return await askToUpdateLanguageServer(languageServer, version, context, logger);
+            } else {
+              return E.right(languageServer);
+            }
+          }
+        )
+      )
+    }
+  }
+
+  return E.left("Invalid configuration for managing the Bend Language Server.");
 }
 
-async function findServerExecutable(logger: Logger, folder?: WorkspaceFolder): Promise<string> {
+async function managedServerExecutableLocation(context: ExtensionContext): Promise<string> {
+  return path.join(await getStoragePath(context), "bin", `${BEND_LS_EXE}${EXT}`);
+}
+
+async function getLanguageServerLatestVersion(context: ExtensionContext, logger: Logger): Promise<E.Either<string, string>> {
+  return pipe(
+    await callCargo(
+      ["search", BEND_LS_EXE],
+      context,
+      logger,
+      "Searching for bend-language-server...",
+      true
+    ),
+    E.map(
+      output => {
+        let version = /".*"/.exec(output)[0];
+        version = version.substring(1, version.length - 1); // quotes
+        return version;
+      }
+    ));
+}
+
+async function askToUpdateLanguageServer(languageServer: string, version: string, context: ExtensionContext, logger: Logger): Promise<E.Either<string, string>> {
+  const decision = await window.showInformationMessage(
+    `There is a new version of the Bend Language Server (${version}). Would you like to install it?`,
+    "Yes",
+    "No"
+  );
+
+  if (decision == "No") {
+    return E.right(languageServer);
+  }
+
+  // Update the language server
+  logger.info(`Installing bend-language-server ${version}`);
+
+  return await installLanguageServer(version, context, logger);
+}
+
+async function installLanguageServer(version: string, context: ExtensionContext, logger: Logger): Promise<E.Either<string, string>> {
+  return pipe(
+    await callCargo(
+      [BEND_LS_EXE, "--version", version, "--root", await getStoragePath(context)],
+      context,
+      logger,
+      "Installing the Bend Language Server...",
+      true
+    ),
+    E.chain(
+      async _output => E.right(await managedServerExecutableLocation(context))
+    )
+  );
+}
+
+async function findConfiguredServerExecutable(logger: Logger, folder?: WorkspaceFolder): Promise<E.Either<string, string>> {
   let executablePath = workspace.getConfiguration("bend").get("serverExecutablePath") as string;
   logger.info(`Trying to find the Bend Language Server binary at ${executablePath}`);
 
-  executablePath = resolvePathPlaceHolders(executablePath, folder);
+  executablePath = resolvePlaceHolders(executablePath, folder);
   logger.info(`Resolved path variables: ${executablePath}`);
 
   if (executableExists(executablePath)) {
-    return executablePath;
+    return E.right(executablePath);
   } else {
-    throw new Error(dedent`
+    return E.left(dedent`
       Could not find the Bend Language Server at ${execPath}.
       Consider changing settings for "bend.manageLanguageServer" or "bend.serverExecutablePath".
       `);
   }
 }
 
-async function findServerExecutableinPATH(_context: ExtensionContext, logger: Logger): Promise<string> {
-  const executable = "bend-language-server";
+async function findExecutableInPATH(executable: string, _context: ExtensionContext, logger: Logger): Promise<E.Either<string, string>> {
   if (executableExists(executable)) {
-    logger.info(`Found server executable in $PATH: ${executable}`);
-    return executable;
+    return E.right(executable);
+  } else {
+    return E.left(`Could not find ${executable} in PATH.`)
   }
-  throw new Error('Could not find the bend-language-server binary in PATH.');
 }
 
-export function resolvePathPlaceHolders(path: string, folder?: WorkspaceFolder) {
-  path = path.replace('${HOME}', os.homedir).replace('${home}', os.homedir).replace(/^~/, os.homedir);
-  if (folder) {
-    path = path.replace('${workspaceFolder}', folder.uri.path).replace('${workspaceRoot}', folder.uri.path);
-  }
-  return path;
-}
-
-export function resolvePATHPlaceHolders(path: string) {
-  return path
+export function resolvePlaceHolders(path: string, folder?: WorkspaceFolder) {
+  path = path
     .replace('${HOME}', os.homedir)
     .replace('${home}', os.homedir)
+    .replace(/^~/, os.homedir)
     .replace('$PATH', process.env.PATH ?? '$PATH')
-    .replace('${PATH}', process.env.PATH ?? '${PATH}');
+    .replace('${PATH}', process.env.PATH ?? '${PATH}')
+    .replace('${CARGO_HOME}', process.env.CARGO_HOME);
+
+  if (folder) {
+    path = path
+      .replace('${workspaceFolder}', folder.uri.path)
+      .replace('${workspaceRoot}', folder.uri.path);
+  }
+  return path;
 }
 
 export function executableExists(exe: string): boolean {
@@ -156,7 +260,7 @@ export function executableExists(exe: string): boolean {
 export function resolveServerEnvironmentPATH(serverEnv: IEnvVars): IEnvVars {
   const pathSep = process.platform === 'win32' ? ';' : ':';
   const path: string[] | null = serverEnv.PATH
-    ? serverEnv.PATH.split(pathSep).map((p) => resolvePATHPlaceHolders(p))
+    ? serverEnv.PATH.split(pathSep).map((p) => resolvePlaceHolders(p))
     : null;
   return {
     ...serverEnv,
@@ -170,38 +274,71 @@ async function getStoragePath(
   return context.globalStorageUri.fsPath;
 }
 
-async function findCargo(_context: ExtensionContext, logger: Logger, folder?: WorkspaceFolder): Promise<string> {
+async function findCargo(_context: ExtensionContext, logger: Logger, folder?: WorkspaceFolder): Promise<E.Either<string, string>> {
   logger.info("Looking for Cargo...");
 
   let cargoExecutable = workspace.getConfiguration("bend").get("cargoExecutablePath") as string;
   if (cargoExecutable) {
     logger.info(`Looking for Cargo in ${cargoExecutable}...`);
-    cargoExecutable = resolvePathPlaceHolders(cargoExecutable, folder);
+    cargoExecutable = resolvePlaceHolders(cargoExecutable, folder);
     logger.info(`Translated the path to ${cargoExecutable}`);
     if (executableExists(cargoExecutable)) {
-      return cargoExecutable;
+      return E.right(cargoExecutable);
     } else {
-      throw new Error(`Could not find Cargo at ${cargoExecutable}`);
+      return E.left(`Could not find Cargo at ${cargoExecutable}`);
     }
   }
 
   if (executableExists("cargo")) {
     // Found it in PATH
-    return "cargo"
+    return E.right("cargo");
   }
 
-  // We'll try to find the `cargo by looking around
+  // We'll try to find the cargo binary by looking around
   logger.info("Probing for Cargo...");
-  cargoExecutable = match(process.platform)
-    .with("win32", () => {
-      // use homedir
-      return ""
-    });
 
+  cargoExecutable = path.join(process.env.CARGO_HOME, ".cargo", "bin", "cargo");
+  if (executableExists(cargoExecutable)) return E.right(cargoExecutable);
+
+  cargoExecutable = path.join(os.homedir(), ".cargo", "bin", "cargo");
+  if (executableExists(cargoExecutable)) return E.right(cargoExecutable);
+
+  return E.left("Could not find the Cargo binary.");
+}
+
+async function callCargo(
+  args: string[],
+  context: ExtensionContext,
+  logger: Logger,
+  title?: string,
+  cancellable?: boolean,
+  callback?: ProcessCallback
+): Promise<E.Either<string, string>> {
+  if (manageLS !== "automatic") {
+    return E.left("Tried to call Cargo when bend.manageLanguageServer is not set to automatic.");
+  }
+
+  return pipe(
+    await findCargo(context, logger),
+    E.match(
+      async (error) => E.left(error),
+      async (cargo) => E.right(
+        await callAsync(
+          cargo,
+          args,
+          logger,
+          undefined,
+          title,
+          cancellable,
+          {},
+          callback
+        ))
+    )
+  )
 }
 
 async function callAsync(
-  binary: string,
+  executablePath: string,
   args: string[],
   logger: Logger,
   dir?: string,
@@ -222,7 +359,7 @@ async function callAsync(
     },
     async (_, token) => {
       return new Promise<string>((resolve, reject) => {
-        const command: string = binary + ' ' + args.join(' ');
+        const command: string = executablePath + ' ' + args.join(' ');
         logger.info(`Executing '${command}' in cwd '${dir ? dir : process.cwd()}'`);
         token.onCancellationRequested(() => {
           logger.warn(`User canceled the execution of '${command}'`);
@@ -231,7 +368,7 @@ async function callAsync(
         // We execute the command in a shell for windows, to allow use .cmd or .bat scripts
         const childProcess = child_process
           .execFile(
-            process.platform === 'win32' ? `"${binary}"` : binary,
+            process.platform === 'win32' ? `"${executablePath}"` : executablePath,
             args,
             { encoding: 'utf8', cwd: dir, shell: process.platform === 'win32', env: newEnv },
             (err, stdout, stderr) => {
